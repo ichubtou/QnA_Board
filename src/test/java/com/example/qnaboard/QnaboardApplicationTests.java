19f9f63f93e90ea8e696cc3408293d64b7bfd1cf
ichubtou <msjj0809@naver.com>
2023-02-02 22:50:38 +0900

    init

---
+++ src/test/java/com/example/qnaboard/QnaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qnaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
