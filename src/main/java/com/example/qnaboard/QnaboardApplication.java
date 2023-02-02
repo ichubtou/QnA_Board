04e2d43cf52a320a0f6337caebf5064fa52dbfb8
ichubtou <msjj0809@naver.com>
2023-02-02 22:51:58 +0900

    init

--- src/main/java/com/example/qnaboard/QnaboardApplication.java
+++ src/main/java/com/example/qnaboard/QnaboardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QnaboardApplication {
+public class QnABoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QnaboardApplication.class, args);
+		SpringApplication.run(QnABoardApplication.class, args);
 	}
 
 }
