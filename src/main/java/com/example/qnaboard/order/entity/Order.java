b7b794ac58da2993fbf88051cb42e1dc66c6bf32
ichubtou <msjj0809@naver.com>
2023-02-04 17:55:15 +0900

    Feat: Order entity 추가

---
+++ src/main/java/com/example/qnaboard/order/entity/Order.java
@@ -0,0 +1,65 @@
+package com.example.qnaboard.order.entity;
+
+import com.example.qnaboard.audit.Auditable;
+import com.example.qnaboard.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Order extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus orderStatus = OrderStatus.ORDER_REQUEST;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.PERSIST)
+    private List<OrderCoffee> orderCoffees = new ArrayList<>();
+
+    public void setMember(Member member) {
+        this.member = member;
+        if(!member.getOrders().contains(this)) {
+            this.member.addOrders(this);
+        }
+    }
+
+    public void setOrderCoffee(OrderCoffee orderCoffee) {
+        this.orderCoffees.add(orderCoffee);
+        if(orderCoffee.getOrder() != this) {
+            orderCoffee.addOrder(this);
+        }
+    }
+
+    public enum OrderStatus {
+        ORDER_REQUEST(1, "주문 요청"),
+        ORDER_CONFIRM(2, "주문 확정"),
+        ORDER_COMPLETE(3, "주문 처리 완료"),
+        ORDER_CANCEL(4, "주문 취소");
+
+        @Getter
+        private int stepNumber;
+
+        @Getter
+        private String stepDescription;
+
+        OrderStatus(int stepNumber, String stepDescription) {
+            this.stepNumber = stepNumber;
+            this.stepDescription = stepDescription;
+        }
+    }
+
+}
